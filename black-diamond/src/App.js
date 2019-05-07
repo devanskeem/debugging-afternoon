c901c7551940339266500cc938b72314b6672f73
Devan Skeem <devanskeem@gmail.com>
2019-05-07 15:03:44 -0600

    finished with black-diamond

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios";
+import axios from 'axios'
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
@@ -12,6 +12,7 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.navigate = this.navigate.bind(this)
   }
   componentDidMount() {
     axios
@@ -22,23 +23,29 @@ class App extends Component {
         });
       });
   }
-  addToCart(item) {
+  addToCart = (item) => {
+    console.log(this.state.cart)
+    let tempCart = [...this.state.cart, item]
     this.setState({
-      cart: [...this.state.cart, item]
+      cart: tempCart
     });
   }
-  removeFromCart(index) {
+  removeFromCart = (index) => {
     let cartCopy = this.state.cart.slice();
     cartCopy.splice(index, 1);
     this.setState({
       cart: cartCopy
     });
   }
-  navigate(location) {
+  navigate = (location) => {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({
+        showCart: true
+      })
     } else {
-      this.state.showCart = false;
+      this.setState(
+        {showCart: false
+        });
     }
   }
   render() {
